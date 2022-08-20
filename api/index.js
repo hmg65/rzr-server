d84766d65f6951bf4e529c293242b3b34d90dc92
Hemant Gupta <gupta.hemant65@gmail.com>
2022-08-20 19:06:59 +0530

    update: payout api working

--- api/index.js
+++ api/index.js
@@ -8,10 +8,11 @@ dotenv.config();
 
 app.use(express.json());
 
+let username = process.env.NODE_APP_USERNAME;
+let password = process.env.NODE_APP_PASSWORD;
+
 app.get("/api/validate/:vpa", async (req, res) => {
 
-  let username = process.env.NODE_APP_USERNAME;
-  let password = process.env.NODE_APP_PASSWORD;
 const vp_add = req.params.vpa;
   const body = {
     vpa: vp_add,
@@ -32,6 +33,45 @@ const vp_add = req.params.vpa;
   const vpa_res = await api_response.json();
   res.json(vpa_res);
 });
+
+
+app.get("/api/payout/:vpa/:amount", async (req, res) => {
+
+const vp_add = req.params.vpa;
+const amt = req.params.amount;
+  const body = {
+    "account_number":"7878780014577958",
+    "amount": amt,
+    "currency":"INR",
+    "mode":"UPI",
+    "purpose":"payout",
+    "fund_account":{
+        "account_type":"vpa",
+        "vpa":{
+            "address": vp_add
+        },
+        "contact":{
+            "name":"Payout for Ripple Txn"
+        }
+    }
+  };
+
+  const api_response = await fetch(
+    "https://api.razorpay.com/v1/payouts",
+    {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic " + btoa(username + ":" + password),
+      },
+    }
+  );
+
+  const vpa_res = await api_response.json();
+  res.json(vpa_res);
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port);
