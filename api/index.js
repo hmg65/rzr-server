80e696cccea730b13bc702f0b9bc9364d2ae6a01
Hemant Gupta <gupta.hemant65@gmail.com>
2022-08-20 19:07:59 +0530

    bug : amount related

--- api/index.js
+++ api/index.js
@@ -12,8 +12,7 @@ let username = process.env.NODE_APP_USERNAME;
 let password = process.env.NODE_APP_PASSWORD;
 
 app.get("/api/validate/:vpa", async (req, res) => {
-
-const vp_add = req.params.vpa;
+  const vp_add = req.params.vpa;
   const body = {
     vpa: vp_add,
   };
@@ -34,44 +33,38 @@ const vp_add = req.params.vpa;
   res.json(vpa_res);
 });
 
-
 app.get("/api/payout/:vpa/:amount", async (req, res) => {
-
-const vp_add = req.params.vpa;
-const amt = req.params.amount;
+  const vp_add = req.params.vpa;
+  const amt = req.params.amount * 100;
   const body = {
-    "account_number":"7878780014577958",
-    "amount": amt,
-    "currency":"INR",
-    "mode":"UPI",
-    "purpose":"payout",
-    "fund_account":{
-        "account_type":"vpa",
-        "vpa":{
-            "address": vp_add
-        },
-        "contact":{
-            "name":"Payout for Ripple Txn"
-        }
-    }
+    account_number: "7878780014577958",
+    amount: amt,
+    currency: "INR",
+    mode: "UPI",
+    purpose: "payout",
+    fund_account: {
+      account_type: "vpa",
+      vpa: {
+        address: vp_add,
+      },
+      contact: {
+        name: "Payout for Ripple Txn",
+      },
+    },
   };
 
-  const api_response = await fetch(
-    "https://api.razorpay.com/v1/payouts",
-    {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Basic " + btoa(username + ":" + password),
-      },
-    }
-  );
+  const api_response = await fetch("https://api.razorpay.com/v1/payouts", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Basic " + btoa(username + ":" + password),
+    },
+  });
 
   const vpa_res = await api_response.json();
   res.json(vpa_res);
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port);
